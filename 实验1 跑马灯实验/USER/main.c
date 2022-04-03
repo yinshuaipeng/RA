316c92e1ab15987db525ee176e1699c1be0a61e1
yinshuaipeng <171256268@qq.com>
2022-04-03 22:09:31 +0800

    第二次

--- 实验1 跑马灯实验/USER/main.c
+++ 实验1 跑马灯实验/USER/main.c
@@ -27,10 +27,10 @@ int main(void)
     {
         HAL_GPIO_WritePin(GPIOB,GPIO_PIN_1,GPIO_PIN_RESET); //LED0对应引脚PB1拉低，亮，等同于LED0(0)
         HAL_GPIO_WritePin(GPIOB,GPIO_PIN_0,GPIO_PIN_SET);   //LED1对应引脚PB0拉高，灭，等同于LED1(1)
-        delay_ms(800);										//延时500ms
+        delay_ms(100);										//延时500ms
         HAL_GPIO_WritePin(GPIOB,GPIO_PIN_1,GPIO_PIN_SET);   //LED0对应引脚PB1拉高，灭，等同于LED0(1)
         HAL_GPIO_WritePin(GPIOB,GPIO_PIN_0,GPIO_PIN_RESET); //LED1对应引脚PB0拉低，亮，等同于LED1(0)
-        delay_ms(800);                                      //延时500ms 
+        delay_ms(100);                                      //延时500ms 
     }
 }
  
