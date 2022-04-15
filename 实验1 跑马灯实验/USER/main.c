02c36eb2d3a0f7f402a8249bd2bebda07de18320
shuaipeng_yin <171256268@qq.com>
2022-04-15 10:25:24 +0800

    两次延时

--- 实验1 跑马灯实验/USER/main.c
+++ 实验1 跑马灯实验/USER/main.c
@@ -25,16 +25,14 @@ int main(void)
     LED_Init();                     //初始化LED  
     while(1)
     {
-		while（1）
-		{
-			
-		}
         HAL_GPIO_WritePin(GPIOB,GPIO_PIN_1,GPIO_PIN_RESET); //LED0对应引脚PB1拉低，亮，等同于LED0(0)
         HAL_GPIO_WritePin(GPIOB,GPIO_PIN_0,GPIO_PIN_SET);   //LED1对应引脚PB0拉高，灭，等同于LED1(1)
         delay_ms(100);										//延时500ms
+				delay_ms(100);
         HAL_GPIO_WritePin(GPIOB,GPIO_PIN_1,GPIO_PIN_SET);   //LED0对应引脚PB1拉高，灭，等同于LED0(1)
         HAL_GPIO_WritePin(GPIOB,GPIO_PIN_0,GPIO_PIN_RESET); //LED1对应引脚PB0拉低，亮，等同于LED1(0)
         delay_ms(100);                                      //延时500ms 
+				delay_ms(100);
     }
 }
  
